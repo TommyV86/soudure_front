719eb191d594c57a895a20dfbdbb521d8c833493
TommyV86 <tommychel@hotmail.fr>
2024-07-04 12:04:37 +0200

    init projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/public/header/header.component';
+import { FooterComponent } from './component/public/footer/footer.component';
+import { MainPageComponent } from './component/public/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
