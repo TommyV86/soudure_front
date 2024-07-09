45c401168eae744e55d12314576d2c8480881cf2
TommyV86 <tommychel@hotmail.fr>
2024-07-09 18:38:21 +0200

    developpement responsive pour format mobile de la page contact, nouvelles fonctions pour les dropdowns de la navbar, de l'affichage d'une carte de detail

--- src/app/component/public/header/header.component.ts
+++ src/app/component/public/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+
+  ngOnInit(): void {
+    this.setupNavbarCollapse();
+  }
+
+  public setupNavbarCollapse(): void {
+    const navItems = document.querySelectorAll('.nav-item');
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+
+    navItems.forEach(item => {
+      item.addEventListener('click', (event) => {
+        const target = event.target as HTMLElement;
+
+        // Vérifie si l'élément cliqué est dans un dropdown
+        if (target.classList.contains('dropdown-toggle') || target.closest('.dropdown-menu')) {
+          return;
+        }
+
+        if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+          const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+            toggle: false
+          });
+          bsCollapse.hide();
+        }
+      });
+    });
+
+    // Ajoutez un écouteur de clic pour fermer le menu burger quand on clique dans un dropdown
+    document.querySelectorAll('.dropdown-item').forEach(item => {
+      item.addEventListener('click', () => {
+        if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+          const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+            toggle: false
+          });
+          bsCollapse.hide();
+        }
+      });
+    });
+  }
+  
+
+  @HostListener('document:click', ['$event'])
+  public onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+    // Vérifie si l'élément cliqué est à l'extérieur du menu burger
+    if (!target.closest('.navbar-collapse') && !target.closest('.navbar-toggler')) {
+      const navbarCollapse = document.getElementById('navbarSupportedContent');
+      if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+          toggle: false
+        });
+        bsCollapse.hide();
+      }
+    }
+  }
 
 }
